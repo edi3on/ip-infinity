f9c34fa8bef0c46cc60612b633762ea21bfe73c2
kaanhar <hrt.kaan@gmail.com>
2024-10-19 23:33:51 -0700

    Update sendToken.ts

--- ts-backend/storyprotocolserver/scripts/sendToken.ts
+++ ts-backend/storyprotocolserver/scripts/sendToken.ts
@@ -1,35 +1,30 @@
-import { ethers } from 'ethers';
+  import { ethers } from 'ethers';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 // Load environment variables from .env file
-const INFURA_API_KEY = process.env.INFURA_API_KEY;
 const PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY;
 const TOKEN_CONTRACT_ADDRESS = process.env.TOKEN_CONTRACT_ADDRESS;
-const RECEIVER_ADDRESS = "0xDa2e0b8bFD702F9cF81B8dFf5F064e7405acf754";
-const AMOUNT = 10;
-
-if (!INFURA_API_KEY || !PRIVATE_KEY || !TOKEN_CONTRACT_ADDRESS || !RECEIVER_ADDRESS || !AMOUNT) {
-  throw new Error("Missing environment variables. Please check your .env file.");
-}
+const POLY_TOKEN_CONTRACT_ADDRESS = process.env.POLY_TOKEN_CONTRACT_ADDRESS;
 
 const provider = new ethers.JsonRpcProvider(`https://testnet.storyrpc.io`);
-const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
+const wallet = new ethers.Wallet(PRIVATE_KEY as string, provider);
+
 
 // ERC-20 Token ABI
 const tokenAbi = [
   "function transfer(address to, uint amount) public returns (bool)",
 ];
 
-export const sendErc20Token = async () => {
+export const sendStoryErc20Token = async (reciepint: string, amount: number, ) => {
   try {
-    const tokenContract = new ethers.Contract(TOKEN_CONTRACT_ADDRESS, tokenAbi, wallet);
+    const tokenContract = new ethers.Contract(TOKEN_CONTRACT_ADDRESS as string, tokenAbi, wallet);
     const decimals = 2;
-    const parsedAmount = ethers.parseUnits(AMOUNT.toString(), decimals);
+    const parsedAmount = ethers.parseUnits(amount.toString(), decimals);
 
     console.log("Sending transaction...");
-    const transaction = await tokenContract.transfer(RECEIVER_ADDRESS, parsedAmount);
+    const transaction = await tokenContract.transfer(reciepint, parsedAmount);
 
     console.log("Waiting for transaction confirmation...");
     const receipt = await transaction.wait();
@@ -40,4 +35,21 @@ export const sendErc20Token = async () => {
   }
 };
 
-//not finished
+export const sendPolyErc20Token = async (reciepint: string, amount: number, ) => {
+  try {
+    const tokenContract = new ethers.Contract(POLY_TOKEN_CONTRACT_ADDRESS as string, tokenAbi, wallet);
+    const decimals = 2;
+    const parsedAmount = ethers.parseUnits(amount.toString(), decimals);
+
+    console.log("Sending transaction...");
+    const transaction = await tokenContract.transfer(reciepint, parsedAmount);
+
+    console.log("Waiting for transaction confirmation...");
+    const receipt = await transaction.wait();
+
+    console.log("Transaction confirmed:", receipt);
+  } catch (error) {
+    console.error("Error sending token:", error);
+  }
+};
+
